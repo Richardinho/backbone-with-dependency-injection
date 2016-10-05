2a1d9d335247124bb57e7180ac146a7c531df031
Richard Hunter <richard@richardhunter.co.uk>
2016-10-05 20:17:51 +0100

    umd

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,4 +1,20 @@
-define(function () {
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function () {
+				return (root.returnExportsGlobal = factory());
+		});
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals
+		root.returnExportsGlobal = factory();
+	}
+}(this, function () {
 
 	'use strict';
 
@@ -121,5 +137,4 @@ define(function () {
 
 	return Injector;
 
-
-});
+}));
