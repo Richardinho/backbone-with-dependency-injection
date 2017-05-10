590528f08c05f557dd2c67274c4eb920176f586d
Richard Hunter <richard@richardhunter.co.uk>
2017-05-10 22:14:22 +0100

    github page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 requirejs.config({
 
 	paths : {
-		'backbone' : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js',
-		'underscore' : 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
-		'jquery' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+		'backbone' : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
+		'underscore' : 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
+		'jquery' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
 		'injector' : '../lib/injector'
 	}
 
